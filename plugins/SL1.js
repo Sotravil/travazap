f20ffc0fee99ad30a6b80891c3bc7511b3e60f45
Sotravil <78595995+Sotravil@users.noreply.github.com>
2021-10-03 15:59:32 -0500

    Update SL1.js

--- plugins/SL1.js
+++ plugins/SL1.js
@@ -3,7 +3,7 @@ let handler = m => m
 let levelling = require('../lib/levelling')
 handler.before = m => {
 	let user = global.db.data.users[m.sender]
-	if (!user.T1) return !0
+	if (!user.autolevelup) return !0
 	let before = user.level * 1
 	while (levelling.canLevelUp(user.level, user.exp, global.multiplier)) user.level++
         
