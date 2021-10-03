a145a9e6fa0f9aa8c0b841aa2cc395f633d80563
Sotravil <78595995+Sotravil@users.noreply.github.com>
2021-10-03 00:02:04 -0500

    Update SL1.js

--- plugins/SL1.js
+++ plugins/SL1.js
@@ -1,1 +1,11 @@
-let handler = async (m, { conn, usedPrefix, command, args, isOwner, isAdmin, isROwner }) => {
+function handler(m) {
+  // Ini cuma contoh, jgn di uncomment -_-
+  // F this.sendContact(m.chat, '62815158600891', 'Nurutomo', m)
+  this.reply(m.chat, m)
+}
+handler.help = ['owner', 'creator']
+handler.tags = ['info']
+
+handler.command = /^(owner|creator)$/i
+
+module.exports = handler
