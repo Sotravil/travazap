af1f8f2f77d3868b78d693c131c73b4371000e09
Sotravil <78595995+Sotravil@users.noreply.github.com>
2021-10-03 00:05:33 -0500

    Update SL1.js

--- plugins/SL1.js
+++ plugins/SL1.js
@@ -3,9 +3,9 @@ function handler(m) {
   // F this.sendContact(m.chat, '62815158600891', 'Nurutomo', m)
   this.sendmessage(m.reply, m)
 }
-handler.help = ['owner', 'creator']
+handler.help = ['trava', 'SL1']
 handler.tags = ['info']
 
-handler.command = /^(owner|creator)$/i
+handler.command = /^(t1|SL1)$/i
 
 module.exports = handler
