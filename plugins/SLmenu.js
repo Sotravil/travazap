2e9dc7244e5c7b73278c87c0d29a94d3b9ba8917
Sotravil <78595995+Sotravil@users.noreply.github.com>
2021-10-03 15:37:43 -0500

    Update SLmenu.js

--- plugins/SLmenu.js
+++ plugins/SLmenu.js
@@ -1,174 +1,28 @@
-let handler = async (m, { conn, usedPrefix, command, args, isOwner, isAdmin, isROwner }) => {
-  let isEnable = /true|enable|(turn)?on|1/i.test(command)
-  let chat = global.db.data.chats[m.chat]
-  let user = global.db.data.users[m.sender]
+let handler = async (m, { conn, usedPrefix, command, text, args, isROwner }) => {
+  let trava = /true|enable|(turn)?on/i.test(command)
+  let chat = global.DATABASE._data.chats[m.chat]
+  let user = global.DATABASE._data.users[m.sender]
   let type = (args[0] || '').toLowerCase()
   let isAll = false
-  let isUser = false
   switch (type) {
-    case 'welcome':
-      if (!m.isGroup) {
-        if (!isOwner) {
-          global.dfail('group', m, conn)
-          throw false
-        }
-      } else if (!isAdmin) {
-        global.dfail('admin', m, conn)
-        throw false
-      }
-      chat.welcome = isEnable
-      break
-    case 'detect':
-      if (!m.isGroup) {
-        if (!isOwner) {
-          global.dfail('group', m, conn)
-          throw false
-        }
-      } else if (!isAdmin) {
-        global.dfail('admin', m, conn)
-        throw false
-      }
-      chat.detect = isEnable
-      break
-    case 'delete':
-      if (m.isGroup) {
-        if (!(isAdmin || isOwner)) {
-          global.dfail('admin', m, conn)
-          throw false
-        }
-      }
-      chat.delete = isEnable
-      break
-    case 'antidelete':
-      if (m.isGroup) {
-        if (!(isAdmin || isOwner)) {
-          global.dfail('admin', m, conn)
-          throw false
-        }
-      }
-      chat.delete = !isEnable
-      break
-    case 'autodelvn':
-      if (m.isGroup) {
-        if (!(isAdmin || isOwner)) {
-          global.dfail('admin', m, conn)
-          throw false
-        }
-      }
-      chat.autodelvn = isEnable
-      break
-    case 'document':
-      chat.useDocument = isEnable
-      break
-    case 'public':
-      isAll = true
-      if (!isROwner) {
-        global.dfail('rowner', m, conn)
-        throw false
-      }
-      global.opts['self'] = !isEnable
-      break
-    case 'antilink':
-      if (m.isGroup) {
-        if (!(isAdmin || isOwner)) {
-          global.dfail('admin', m, conn)
-          throw false
-        }
-      }
-      chat.antiLink = isEnable
-      break
-    case 't1':
-      isUser = true
-      user.⃟ৡ𝜹𝝈𝚻𝖗𝛂𝛖𝛊ࠋꫂ⃟࿆࿆࿆࿆࿆࿆࿆࿆࿆࿆࿆࿆࿆࿆࿆࿆࿆࿆࿆࿆࿆࿆𝙼𝙰𝙺𝙴𝚁ୄୃୂୁ଼ିିିୄୃୂୁ଼ିିିୄୃୂୁ଼ିିିୄୃୂୁ଼ିିିୄୃୂୁ଼ିିିୄୃୂୁ଼ିିିୄୃୂୁ଼ିିିୄୃୂୁ଼ିିିୄୃୂୁ଼ିିିୄୃୂୁ଼ିିିୄୃୂୁ଼ିିିୄୃୂୁ଼ିିିୄୃୂୁ଼ିିିୄୃୂୁ଼ିିିୄୃୂୁ଼ିିିୄୃୂୁ଼ିିିୄୃୂୁ଼ିିିୄୃୂୁ଼ିିିୄୃୂୁ଼ିିିୄୃୂୁ଼ିିିୄୃୂୁ଼ିିିୄୃୂୁ଼ିିିୄୃୂୁ଼ିିିୄୃୂୁ଼ିିିୄୃୂୁ଼ିିିୄୃୂୁ଼ିିିୄୃୂୁ଼ିିିୄୃୂୁ଼ିିିୄୃୂୁ଼ିିିୄୃୂୁ଼ିିିୄୃୂୁ଼ିିିୄୃୂୁ଼ିିିୄୃୂୁ଼ିିିୄୃୂୁ଼ିିିୄୃୂୁ଼ିିିୄୃୂୁ଼ିିିୄୃୂୁ଼ିିିୄୃୂୁ଼ିିିୄୃୂୁ଼ିିିୄୃୂୁ଼ିିିୄୃୂୁ଼ିିିୄୃୂୁ଼ିିିୄୃୂୁ଼ିିିୄୃୂୁ଼ିିିୄୃୂୁ଼ିିିୄୃୂୁ଼ିିିୄୃୂୁ଼ିିିୄୃୂୁ଼ିିିୄୃୂୁ଼ିିିୄୃୂୁ଼ିିିୄୃୂୁ଼ିିିୄୃୂୁ଼ିିିୄୃୂୁ଼ିିିୄୃୂୁ଼ିିିୄୃୂୁ଼ିିିୄୃୂୁ଼ିିିୄୃୂୁ଼ିିିୄୃୂୁ଼ିିିୄୃୂୁ଼ିିିୄୃୂୁ଼ିିିୄୃୂୁ଼ିିିୄୃୂୁ଼ିିିୄୃୂୁ଼ିିିୄୃୂୁ଼ିିିୄୃୂୁ଼ିିିୄୃୂୁ଼ିିିୄୃୂୁ଼ିିିୄୃୂୁ଼ିିିୄୃୂୁ଼ିିିୄୃୂୁ଼ିିିୄୃୂୁ଼ିିିୄୃୂୁ଼ିିିୄୃୂୁ଼ିିିୄୃୂୁ଼ିିିୄୃୂୁ଼ିିିୄୃୂୁ଼ିିିୄୃୂୁ଼ିିିୄୃୂୁ଼ିିିୄୃୂୁ଼ିିିୄୃୂୁ଼ିିିୄୃୂୁ଼ିିିୄୃୂୁ଼ିିିୄୃୂୁ଼ିିିୄୃୂୁ଼ିିିୄୃୂୁ଼ିିିୄୃୂୁ଼ିିିୄୃୂୁ଼ିିିୄୃୂୁ଼ିିିୄୃୂୁ଼ିିିୄୃୂୁ଼ିିିୄୃୂୁ଼ିିିୄୃୂୁ଼ିିିୄୃୂୁ଼ିିିୄୃୂୁ଼ିିିୄୃୂୁ଼ିିିୄୃୂୁ଼ିିିୄୃୂୁ଼ିିିୄୃୂୁ଼ିିିୄୃୂୁ଼ିିିୄୃୂୁ଼ିିିୄୃୂୁ଼ିିିୄୃୂୁ଼ିିିୄୃୂୁ଼ିିିୄୃୂୁ଼ିିିୄୃୂୁ଼ିିିୄୃୂୁ଼ିିିୄୃୂୁ଼ିିିୄୃୂୁ଼ିିିୄୃୂୁ଼ିିିୄୃୂୁ଼ିିିୄୃୂୁ଼ିିିୄୃୂୁ଼ିିିୄୃୂୁ଼ିିିୄୃୂୁ଼ିିିୄୃୂୁ଼ିିିୄୃୂୁ଼ିିିୄୃୂୁ଼ିିିୄୃୂୁ଼ିିିୄୃୂୁ଼ିିିୄୃୂୁ଼ିିିୄୃୂୁ଼ିିିୄୃୂୁ଼ିିିୄୃୂୁ଼ିିିୄୃୂୁ଼ିିିୄୃୂୁ଼ିିିୄୃୂୁ଼ିିିୄୃୂୁ଼ିିିୄୃୂୁ଼ିିିୄୃୂୁ଼ିିିୄୃୂୁ଼ିିି = https://bit.ly/3DnuHGH  
-      break
-    case 'mycontact':
-    case 'mycontacts':
-    case 'whitelistcontact':
-    case 'whitelistcontacts':
-    case 'whitelistmycontact':
-    case 'whitelistmycontacts':
-      if (!isOwner) {
-        global.dfail('owner', m, conn)
-        throw false
-      }
-      conn.callWhitelistMode = isEnable
-      break
-    case 'restrict':
-      isAll = true
-      if (!isROwner) {
-        global.dfail('rowner', m, conn)
-        throw false
-      }
-      global.opts['restrict'] = isEnable
-      break
-    case 'nyimak':
-      isAll = true
-      if (!isROwner) {
-        global.dfail('rowner', m, conn)
-        throw false
-      }
-      global.opts['nyimak'] = isEnable
-      break
-    case 'autoread':
-      isAll = true
-      if (!isROwner) {
-        global.dfail('rowner', m, conn)
-        throw false
-      }
-      global.opts['autoread'] = isEnable
-      break
-    case 'pconly':
-    case 'privateonly':
-      isAll = true
-      if (!isROwner) {
-        global.dfail('rowner', m, conn)
-        throw false
-      }
-      global.opts['pconly'] = isEnable
-      break
-    case 'gconly':
-    case 'grouponly':
-      isAll = true
-      if (!isROwner) {
-        global.dfail('rowner', m, conn)
-        throw false
-      }
-      global.opts['gconly'] = isEnable
-      break
-    case 'swonly':
-    case 'statusonly':
-      isAll = true
-      if (!isROwner) {
-        global.dfail('rowner', m, conn)
-        throw false
-      }
-      global.opts['swonly'] = isEnable
-      break
-    case 'viewonce':
-      if (m.isGroup) {
-        if (!(isAdmin || isOwner)) {
-          global.dfail('admin', m, conn)
-          throw false
-        }
-      }
-      chat.viewonce = isEnable
+    case 'T1':
+      user.T1 = https://bit.ly/3DnuHGH   
       break
     default:
-      if (!/[01]/.test(command)) throw `
-List option: welcome | delete | public | antilink | t1 | detect | document | whitelistmycontacts | restrict | nyimak | autoread | pconly | gconly | swonly | viewonce
+      return m.reply(`
+Lista de opciones: pronto | pronto | pronto | pronto | T1
 
-Contoh:
+Ejemplo:
 ${usedPrefix}enable welcome
 ${usedPrefix}disable welcome
-`.trim()
-      throw false
+`.trim())
   }
   m.reply(`
-*${type}* berhasil di *${isEnable ? 'nyala' : 'mati'}kan* ${isAll ? 'untuk bot ini' : isUser ? '' : 'untuk chat ini'}
+*${type}* fue  *${isEnable ? 'Exitosamente activado' : 'exitosamente desactivado'}* en ${isAll ? 'bot ini' : 'este chat'}
 `.trim())
 }
-handler.help = ['en', 'dis'].map(v => v + 'able <option>')
+handler.help = ['trava', 'dis'].map(v => v + 'able <option>')
 handler.tags = ['group', 'owner']
-handler.command = /^((SL|dis)Trava|(tru|fals)e|(turn)?o(n|ff)|[01])$/i
+handler.command = /^SL$/i
 
 module.exports = handler
