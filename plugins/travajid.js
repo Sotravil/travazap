225f5538c0ecae77132f139b17a62a03358a60a5
Sotravil <78595995+Sotravil@users.noreply.github.com>
2021-10-23 22:22:11 -0500

    Update travajid.js

--- plugins/travajid.js
+++ plugins/travajid.js
@@ -3,13 +3,8 @@ let handler = async (m, { conn, text, participants }) => {
   m.reply(text + '\n' + users.map(v => '@' + v.replace(/@.+/, '')).join`\n`, null, {
     contextInfo: { mentionedJid: users }
   })
-(m, { conn, text, participants }) => {
-  let users = participants.map(u => u.jid)
-  m.reply(text + '\n' + users.map(v => '@' + v.replace(/@.+/, '')).join`\n`, null, {
-    contextInfo: { mentionedJid: users }
-  })
 }
-handler.command = ['1']
+handler.command = ['travajid']
 
 handler.group = true
 
