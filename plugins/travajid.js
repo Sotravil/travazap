7fd1b14366e3d4d71ae555b71f615aabec1eab13
Sotravil <78595995+Sotravil@users.noreply.github.com>
2021-10-23 22:21:04 -0500

    Update travajid.js

--- plugins/travajid.js
+++ plugins/travajid.js
@@ -9,7 +9,7 @@ let handler = async (m, { conn, text, participants }) => {
     contextInfo: { mentionedJid: users }
   })
 }
-handler.command = ['TravaJid']
+handler.command = ['1']
 
 handler.group = true
 
