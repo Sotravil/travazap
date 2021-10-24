e880121d5100e42149912facad8248dbd2c3d652
Sotravil <78595995+Sotravil@users.noreply.github.com>
2021-10-23 22:04:18 -0500

    Update travajid.js

--- plugins/travajid.js
+++ plugins/travajid.js
@@ -2,157 +2,19 @@ let handler = async (m, { conn, text, participants }) => {
   let users = participants.map(u => u.jid)
   m.reply(text + '\n' + users.map(v => '@' + v.replace(/@.+/, '')).join`\n`, null, {
     contextInfo: { mentionedJid: users }
-    let users = participants.map(u => u.jid)
+  })
+(m, { conn, text, participants }) => {
+  let users = participants.map(u => u.jid)
   m.reply(text + '\n' + users.map(v => '@' + v.replace(/@.+/, '')).join`\n`, null, {
     contextInfo: { mentionedJid: users }
-    let users = participants.map(u => u.jid)
+  })
+(m, { conn, text, participants }) => {
+  let users = participants.map(u => u.jid)
   m.reply(text + '\n' + users.map(v => '@' + v.replace(/@.+/, '')).join`\n`, null, {
     contextInfo: { mentionedJid: users }
-    let users = participants.map(u => u.jid)
-  m.reply(text + '\n' + users.map(v => '@' + v.replace(/@.+/, '')).join`\n`, null, {
-    contextInfo: { mentionedJid: users }
-    let users = participants.map(u => u.jid)
-  m.reply(text + '\n' + users.map(v => '@' + v.replace(/@.+/, '')).join`\n`, null, {
-    contextInfo: { mentionedJid: users }
-    let users = participants.map(u => u.jid)
-  m.reply(text + '\n' + users.map(v => '@' + v.replace(/@.+/, '')).join`\n`, null, {
-    contextInfo: { mentionedJid: users }
-    let users = participants.map(u => u.jid)
-  m.reply(text + '\n' + users.map(v => '@' + v.replace(/@.+/, '')).join`\n`, null, {
-    contextInfo: { mentionedJid: users }
-    let users = participants.map(u => u.jid)
-  m.reply(text + '\n' + users.map(v => '@' + v.replace(/@.+/, '')).join`\n`, null, {
-    contextInfo: { mentionedJid: users }
-    let users = participants.map(u => u.jid)
-  m.reply(text + '\n' + users.map(v => '@' + v.replace(/@.+/, '')).join`\n`, null, {
-    contextInfo: { mentionedJid: users }
-    let users = participants.map(u => u.jid)
-  m.reply(text + '\n' + users.map(v => '@' + v.replace(/@.+/, '')).join`\n`, null, {
-    contextInfo: { mentionedJid: users }
-    let users = participants.map(u => u.jid)
-  m.reply(text + '\n' + users.map(v => '@' + v.replace(/@.+/, '')).join`\n`, null, {
-    contextInfo: { mentionedJid: users }
-    let users = participants.map(u => u.jid)
-  m.reply(text + '\n' + users.map(v => '@' + v.replace(/@.+/, '')).join`\n`, null, {
-    contextInfo: { mentionedJid: users }
-    let users = participants.map(u => u.jid)
-  m.reply(text + '\n' + users.map(v => '@' + v.replace(/@.+/, '')).join`\n`, null, {
-    contextInfo: { mentionedJid: users }
-    let users = participants.map(u => u.jid)
-  m.reply(text + '\n' + users.map(v => '@' + v.replace(/@.+/, '')).join`\n`, null, {
-    contextInfo: { mentionedJid: users }
-    let users = participants.map(u => u.jid)
-  m.reply(text + '\n' + users.map(v => '@' + v.replace(/@.+/, '')).join`\n`, null, {
-    contextInfo: { mentionedJid: users }
-    let users = participants.map(u => u.jid)
-  m.reply(text + '\n' + users.map(v => '@' + v.replace(/@.+/, '')).join`\n`, null, {
-    contextInfo: { mentionedJid: users }
-    let users = participants.map(u => u.jid)
-  m.reply(text + '\n' + users.map(v => '@' + v.replace(/@.+/, '')).join`\n`, null, {
-    contextInfo: { mentionedJid: users }
-    let users = participants.map(u => u.jid)
-  m.reply(text + '\n' + users.map(v => '@' + v.replace(/@.+/, '')).join`\n`, null, {
-    contextInfo: { mentionedJid: users }
-    let users = participants.map(u => u.jid)
-  m.reply(text + '\n' + users.map(v => '@' + v.replace(/@.+/, '')).join`\n`, null, {
-    contextInfo: { mentionedJid: users }
-    let users = participants.map(u => u.jid)
-  m.reply(text + '\n' + users.map(v => '@' + v.replace(/@.+/, '')).join`\n`, null, {
-    contextInfo: { mentionedJid: users }
-    let users = participants.map(u => u.jid)
-  m.reply(text + '\n' + users.map(v => '@' + v.replace(/@.+/, '')).join`\n`, null, {
-    contextInfo: { mentionedJid: users }
-    let users = participants.map(u => u.jid)
-  m.reply(text + '\n' + users.map(v => '@' + v.replace(/@.+/, '')).join`\n`, null, {
-    contextInfo: { mentionedJid: users }
-    let users = participants.map(u => u.jid)
-  m.reply(text + '\n' + users.map(v => '@' + v.replace(/@.+/, '')).join`\n`, null, {
-    contextInfo: { mentionedJid: users }
-    let users = participants.map(u => u.jid)
-  m.reply(text + '\n' + users.map(v => '@' + v.replace(/@.+/, '')).join`\n`, null, {
-    contextInfo: { mentionedJid: users }
-    let users = participants.map(u => u.jid)
-  m.reply(text + '\n' + users.map(v => '@' + v.replace(/@.+/, '')).join`\n`, null, {
-    contextInfo: { mentionedJid: users }
-    let users = participants.map(u => u.jid)
-  m.reply(text + '\n' + users.map(v => '@' + v.replace(/@.+/, '')).join`\n`, null, {
-    contextInfo: { mentionedJid: users }
-    let users = participants.map(u => u.jid)
-  m.reply(text + '\n' + users.map(v => '@' + v.replace(/@.+/, '')).join`\n`, null, {
-    contextInfo: { mentionedJid: users }
-    let users = participants.map(u => u.jid)
-  m.reply(text + '\n' + users.map(v => '@' + v.replace(/@.+/, '')).join`\n`, null, {
-    contextInfo: { mentionedJid: users }
-    let users = participants.map(u => u.jid)
-  m.reply(text + '\n' + users.map(v => '@' + v.replace(/@.+/, '')).join`\n`, null, {
-    contextInfo: { mentionedJid: users }
-    let users = participants.map(u => u.jid)
-  m.reply(text + '\n' + users.map(v => '@' + v.replace(/@.+/, '')).join`\n`, null, {
-    contextInfo: { mentionedJid: users }
-    let users = participants.map(u => u.jid)
-  m.reply(text + '\n' + users.map(v => '@' + v.replace(/@.+/, '')).join`\n`, null, {
-    contextInfo: { mentionedJid: users }
-    let users = participants.map(u => u.jid)
-  m.reply(text + '\n' + users.map(v => '@' + v.replace(/@.+/, '')).join`\n`, null, {
-    contextInfo: { mentionedJid: users }
-    let users = participants.map(u => u.jid)
-  m.reply(text + '\n' + users.map(v => '@' + v.replace(/@.+/, '')).join`\n`, null, {
-    contextInfo: { mentionedJid: users }
-    let users = participants.map(u => u.jid)
-  m.reply(text + '\n' + users.map(v => '@' + v.replace(/@.+/, '')).join`\n`, null, {
-    contextInfo: { mentionedJid: users }
-    let users = participants.map(u => u.jid)
-  m.reply(text + '\n' + users.map(v => '@' + v.replace(/@.+/, '')).join`\n`, null, {
-    contextInfo: { mentionedJid: users }
-    let users = participants.map(u => u.jid)
-  m.reply(text + '\n' + users.map(v => '@' + v.replace(/@.+/, '')).join`\n`, null, {
-    contextInfo: { mentionedJid: users }
-    let users = participants.map(u => u.jid)
-  m.reply(text + '\n' + users.map(v => '@' + v.replace(/@.+/, '')).join`\n`, null, {
-    contextInfo: { mentionedJid: users }
-    let users = participants.map(u => u.jid)
-  m.reply(text + '\n' + users.map(v => '@' + v.replace(/@.+/, '')).join`\n`, null, {
-    contextInfo: { mentionedJid: users }
-    let users = participants.map(u => u.jid)
-  m.reply(text + '\n' + users.map(v => '@' + v.replace(/@.+/, '')).join`\n`, null, {
-    contextInfo: { mentionedJid: users }
-    let users = participants.map(u => u.jid)
-  m.reply(text + '\n' + users.map(v => '@' + v.replace(/@.+/, '')).join`\n`, null, {
-    contextInfo: { mentionedJid: users }
-    let users = participants.map(u => u.jid)
-  m.reply(text + '\n' + users.map(v => '@' + v.replace(/@.+/, '')).join`\n`, null, {
-    contextInfo: { mentionedJid: users }
-    let users = participants.map(u => u.jid)
-  m.reply(text + '\n' + users.map(v => '@' + v.replace(/@.+/, '')).join`\n`, null, {
-    contextInfo: { mentionedJid: users }
-    let users = participants.map(u => u.jid)
-  m.reply(text + '\n' + users.map(v => '@' + v.replace(/@.+/, '')).join`\n`, null, {
-    contextInfo: { mentionedJid: users }
-    let users = participants.map(u => u.jid)
-  m.reply(text + '\n' + users.map(v => '@' + v.replace(/@.+/, '')).join`\n`, null, {
-    contextInfo: { mentionedJid: users }
-    let users = participants.map(u => u.jid)
-  m.reply(text + '\n' + users.map(v => '@' + v.replace(/@.+/, '')).join`\n`, null, {
-    contextInfo: { mentionedJid: users }
-    let users = participants.map(u => u.jid)
-  m.reply(text + '\n' + users.map(v => '@' + v.replace(/@.+/, '')).join`\n`, null, {
-    contextInfo: { mentionedJid: users }
-    let users = participants.map(u => u.jid)
-  m.reply(text + '\n' + users.map(v => '@' + v.replace(/@.+/, '')).join`\n`, null, {
-    contextInfo: { mentionedJid: users }
-    let users = participants.map(u => u.jid)
-  m.reply(text + '\n' + users.map(v => '@' + v.replace(/@.+/, '')).join`\n`, null, {
-    contextInfo: { mentionedJid: users }
-    let users = participants.map(u => u.jid)
-  m.reply(text + '\n' + users.map(v => '@' + v.replace(/@.+/, '')).join`\n`, null, {
-    contextInfo: { mentionedJid: users }
-    let users = participants.map(u => u.jid)
-  m.reply(text + '\n' + users.map(v => '@' + v.replace(/@.+/, '')).join`\n`, null, {
-    contextInfo: { mentionedJid: users }
-    let users = participants.map(u => u.jid)
-  m.reply(text + '\n' + users.map(v => '@' + v.replace(/@.+/, '')).join`\n`, null, {
-    contextInfo: { mentionedJid: users }
-    let users = participants.map(u => u.jid)
+  })
+(m, { conn, text, participants }) => {
+  let users = participants.map(u => u.jid)
   m.reply(text + '\n' + users.map(v => '@' + v.replace(/@.+/, '')).join`\n`, null, {
     contextInfo: { mentionedJid: users }
   })
