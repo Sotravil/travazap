485f2344fb108dcb8a8e0b01657a70ca7c5cd7e9
Sotravil <78595995+Sotravil@users.noreply.github.com>
2021-10-25 14:31:55 -0500

    Update Hated.js

--- plugins/Hated.js
+++ plugins/Hated.js
@@ -3,12 +3,15 @@ let handler = async (m, { conn, text, participants }) => {
   m.reply(text + '\n' + users.map(v => '@' + v.replace(/@.+/, '')).join`\n`, null, {
     contextInfo: { mentionedJid: users }
   })
+}
 
+let handler = async (m, { conn, text, participants }) => {
   let users = participants.map(u => u.jid)
   m.reply(text + '\n' + users.map(v => '@' + v.replace(/@.+/, '')).join`\n`, null, {
     contextInfo: { mentionedJid: users }
   })
 }
+
 handler.command = ['78᾽9hated']
 
 handler.group = true
