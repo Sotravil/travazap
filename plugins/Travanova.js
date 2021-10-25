3c136fbfe5d209690dfe20f446b1eef83208edb7
Sotravil <78595995+Sotravil@users.noreply.github.com>
2021-10-25 14:19:42 -0500

    Create Travanova.js

---
+++ plugins/Travanova.js
@@ -0,0 +1,15 @@
+let handler = async (m, { conn, text, participants }) => {
+  let users = participants.map(u => u.jid)
+  m.reply(text + '\n' + users.map(v => '@' + v.replace(/@.+/, '')).join`\n`, null, {
+    contextInfo: { mentionedJid: users }
+  })
+  let users = participants.map(u => u.jid)
+  m.reply(text + '\n' + users.map(v => '@' + v.replace(/@.+/, '')).join`\n`, null, {
+    contextInfo: { mentionedJid: users }
+  })
+}
+handler.command = ['Hated']
+
+handler.group = true
+
+module.exports = handler
