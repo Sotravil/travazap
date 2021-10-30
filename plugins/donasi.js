4c79086dff7db40d1e2062de81903157c2bb30fc
Sotravil <78595995+Sotravil@users.noreply.github.com>
2021-10-30 12:20:22 -0500

    Update donasi.js

--- plugins/donasi.js
+++ plugins/donasi.js
@@ -1,14 +1,8 @@
 let handler = async m => m.reply(`
-╭─「 Donasi • Pulsa 」
-│ • Indosat Ooredoo [081515860089]
-│ • Telkomsel [081334177340]
+╭─「 Donations • Paypal 」
+│ • https://www.paypal.me/Sotravil
 ╰────
-
-╭─「 Donasi • Non Pulsa 」
-│ • Gopay, OVO, Dana [081515860089]
-│ • https://saweria.co/Nurutomo
-╰────
-`.trim()) // Tambah sendiri kalo mau
+`.trim()) // Agréguese si quiere 
 handler.help = ['donasi']
 handler.tags = ['info']
 handler.command = /^dona(te|si)$/i
