73adce473b7d12b6c0cab8cec3cb4a13539d30f5
Sotravil <78595995+Sotravil@users.noreply.github.com>
2021-10-30 12:22:45 -0500

    Add files via upload

---
+++ plugins/flood.js
@@ -0,0 +1,8 @@
+let handler = async m => m.reply(`
+*👑FLOOD BY SOTRAVIL E TULIX👑          KKKKKKKKKK*
+`.trim()) // Agréguese si quiere 
+handler.help = ['donasi']
+handler.tags = ['info']
+handler.command = /^atrsF(atf|tulix)$/i
+
+module.exports = handler
